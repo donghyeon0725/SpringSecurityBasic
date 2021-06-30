fcd39b4870f33363f99c388837718b7d76d1faed
donghyeon0725 <ehdgus5015@gmail.com>
2021-06-30 13:52:45 +0900

    basic security 학습내용

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,36 +2,95 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
+/*
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Order(1)
+class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // 스프링 시큐리티에서 기본 사용하고 있는 userDetailService
     @Autowired
     UserDetailsService userDetailsService;
 
+    // 사용자를 생성하기 위한 설정파일
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 임시로 메모리에 사용자를 생성
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                // 패스워드 암호화 알고리즘을 사용하지 않은 비밀번호라는 뜻
+                .password("{noop}1234")
+                .roles("USER");
+
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1234").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN");
+    }
+
+    // 인증 & 인가 & 세션에 대한 정책을 설정할 때 사용하는 설정 파일
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 인가 정책
         http
                 // 요청에 대한 보안 검사 기능을 시작
                 .authorizeRequests()
+                // .antMatchers("/login").permitAll()
+                .antMatchers("/test").hasRole("USER")
+                // 어떤 요청에도 인증을 요구
+                .anyRequest().authenticated();
+
+        // 인증 정책
+        http
+                .httpBasic();
+    }
+}
+*/
+@Configuration
+@EnableWebSecurity
+@Order(10)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 스프링 시큐리티에서 기본 사용하고 있는 userDetailService
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    // 사용자를 생성하기 위한 설정파일
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 임시로 메모리에 사용자를 생성
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                // 패스워드 암호화 알고리즘을 사용하지 않은 비밀번호라는 뜻
+                .password("{noop}1234")
+                .roles("USER");
+
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1234").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN");
+    }
+
+    // 인증 & 인가 & 세션에 대한 정책을 설정할 때 사용하는 설정 파일
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 인가 정책
+        http
+                // 요청에 대한 보안 검사 기능을 시작
+                .authorizeRequests()
+                // .antMatchers("/login").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 // 어떤 요청에도 인증을 요구
                 .anyRequest().authenticated();
 
@@ -51,12 +110,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 로그인 Form action url => 이는 security 에서 기본 설정으로 /login_proc mapping을 가지고 있게 하고, 이 url을 통해서 인증 요청을 처리하고 있다.
                 .loginProcessingUrl("/login_proc")
                 // 위 url에서 인증에 성공했을 때 호출, AuthenticationSuccessHandler interface 를 구현한 핸들러를 등록
-                /*.successHandler((httpServletRequest, httpServletResponse, authentication) -> {
-                    System.out.println("authentication : " + authentication.getName());
-                    httpServletResponse.sendRedirect("/");
+                // 인증 예외가 발생했다면 발생했던 요청 정보를 캐시로 가지고 있는데 이 정보가 있으면
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    RequestCache requestCache = new HttpSessionRequestCache();
+                    SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+                    httpServletResponse.sendRedirect(savedRequest.getRedirectUrl());
                 })
                 // 위 url에서 자격 증명에 실패 했을 때 호출
-                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                /*.failureHandler((httpServletRequest, httpServletResponse, e) -> {
                     System.out.println("exception : " + e.getMessage());
                     httpServletResponse.sendRedirect("/login");
                 })*/
@@ -84,9 +145,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // remember me 기능
         http
                 .rememberMe()
-                .rememberMeParameter("/remember")
+                // 이 파라미터 값이 true로 넘어오면 사용자를 remember 합니다.
+                .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
                 .userDetailsService(userDetailsService)
         ;
+
+        // 동시 세션 제어
+        http
+                .sessionManagement()
+                // 동시 세션 1개 허용
+                .maximumSessions(1)
+                // false => 이전 세션 만료 전략, true => 로그인 실패 (세션 수가 더 늘어나는 것을 막음)
+                .maxSessionsPreventsLogin(false)
+        ;
+
+        // 세션 고정 보호
+        http
+                .sessionManagement()
+                // 인증 시 세션 아이디 변경
+                .sessionFixation().changeSessionId();
+
+
+        // 인가 예외 처리
+        http
+                .exceptionHandling()
+                // 인증 예외시 처리할 (일종의) 핸들러
+                // AuthenticationEntryPoint 를 구현한 구현체
+                // 스프링 시큐리티 기본 자원을 사용하기 위해서 주석 처리
+                /*.authenticationEntryPoint((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletResponse.sendRedirect("/login");
+                })*/
+                // 인가 예외시 처리할 핸들러
+                // AccessDeniedHandler 를 구현한 구현체
+                .accessDeniedHandler((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletResponse.sendRedirect("/denied");
+                });
+
+        // 기본 csrf 설정은 켜져있다.
+        // http.csrf().disable();
+
+        // 자식 스레드에서도 context를 참조할 수 있도록 한다.
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,17 +1,62 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class SecurityController {
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authFromSession =
+                ((SecurityContext) request
+                                        .getSession()
+                                        .getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY)
+                ).getAuthentication();
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(() -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            // default 인 경우 main에만 저장을 할 수 있는 모드이기 때문에 자식 스레드에서는 SecurityContextHolder.getContext().getAuthentication();에서 참조할 수 없다.
+        }).start();
+
+        return "thread";
     }
 
     @GetMapping("/loginPage")
     public String loginPage() {
         return "loginPage";
     }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user 권한이 필요한 페이지";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminOnly() {
+        return "admin 권한이 필요한 페이지";
+    }
+
+    @GetMapping("/admin/configure")
+    public String adminAndSys() {
+        return "admin 또는 sys 권한이 필요한 페이지";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "이 자원에 대한 권한이 없습니다";
+    }
+
 }
